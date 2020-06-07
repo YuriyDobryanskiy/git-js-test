f2d6711739f13599aeea43a0758128ee1c27695a
Dobryanskiy <juriydobryansky@gmail.com>
2020-06-07 19:59:00 +0300

    first js commit

---
+++ person.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let name = 'login';
+let age = 'age';
+let pass;
+let person={
+    [name]:'john',
+    [age]:30,
+    [pass]:'dcwe'
+}
+
+console.log(person);
+console.log(person[name]);
+console.log(person[age]);
