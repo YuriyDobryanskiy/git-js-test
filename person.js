c21ce1b85e9cbb0a78cacdcb27556699ce3dda4d
Dobryanskiy <juriydobryansky@gmail.com>
2020-06-07 22:47:39 +0300

    HW#008

--- person.js
+++ person.js
@@ -15,3 +15,32 @@ let person={
 console.log(person);
 console.log(person[name]);
 console.log(person[age]);
+
+///////////// HW# 008
+
+let money = prompt("Ваш бюджет на месяц?"),
+	time = prompt("Введите дату в формате YYYY-MM-DD");
+	
+let appData = {
+	budjet : money,
+	timeDate : time,
+	expenses : {
+	},
+	optionalExpenses : {	
+	},
+	income : [],
+	savings : false
+};	
+
+//4
+var i = 1;
+do {	
+	var que1 = prompt("Введите обязательную статью расходов в этом месяце"),
+		que2 = prompt("Во сколько обойдется?");
+	
+	i++;
+} while (i<=2);
+appData.expenses[que1] = que2;
+
+//5
+alert("бюджет на 1 день : " + appData.budjet/30);
